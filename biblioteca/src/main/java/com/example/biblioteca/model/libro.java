a39b69f314709b24aaa1228e79df4b545980a791
Felipe <fe.becerrav@duocuc.cl>
2025-04-09 20:15:01 -0400

    se agregó model libro

---
+++ biblioteca/src/main/java/com/example/biblioteca/model/libro.java
@@ -0,0 +1,19 @@
+package com.example.biblioteca.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class libro {
+
+private int id;
+private String isbn;
+private String titulo;
+private String editorial;
+private int fechaPublicacion;
+private String autor;
+
+}
